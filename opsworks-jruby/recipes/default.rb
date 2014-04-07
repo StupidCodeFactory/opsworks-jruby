9a48e9b33a0bc8b97b91eb4ff7e617bbac55756f
Yann Marquet <ymarquet@gmail.com>
2014-04-07 12:11:45 +0100

    add rbenv and install jruby

--- opsworks-jruby/recipes/default.rb
+++ opsworks-jruby/recipes/default.rb
@@ -3,6 +3,19 @@
 # Recipe:: default
 #
 # Copyright (C) 2014 YOUR_NAME
-# 
+#
 # All rights reserved - Do Not Redistribute
 #
+include_recipe "rbenv::default"
+include_recipe "rbenv::ruby_build"
+include_recipe "rbenv::rbenv_vars"
+
+rbenv_ruby "jruby-1.7.11" do
+  global true
+end
+
+%w(bundler rake).each do |g|
+  rbenv_gem g do
+    ruby_version "jruby-1.7.11"
+  end
+end
