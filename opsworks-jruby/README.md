0dd8fc595c2af6aedbd7525e443eeb1209b2d2da
Yann Marquet <ymarquet@gmail.com>
2014-04-07 11:05:56 +0100

    add base jrbuy coobook

---
+++ opsworks-jruby/README.md
@@ -0,0 +1,13 @@
+# opsworks-jruby cookbook
+
+# Requirements
+
+# Usage
+
+# Attributes
+
+# Recipes
+
+# Author
+
+Author:: YOUR_NAME (<YOUR_EMAIL>)
---
+++ opsworks-jruby/metadata.rb
@@ -0,0 +1,10 @@
+name             'opsworks-jruby'
+maintainer       'YOUR_NAME'
+maintainer_email 'YOUR_EMAIL'
+license          'All rights reserved'
+description      'Installs/Configures opsworks-jruby'
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
+version          '0.1.0'
+
+depends          'java'
+depends          'rbenv'
